3833a73cc3e0751ec1f778b4a4496a38c8f8ef0d
Maxim Shchetynin <maxim@lutz.fritz.box>
2014-03-03 21:16:13 +0100

    Initial Commit

---
+++ Numeracy/NumeracyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NumeracyAppDelegate.h
+//  Numeracy
+//
+//  Created by Maxim Shchetynin on 03.03.14.
+//  Copyright (c) 2014 Elena Kukuruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NumeracyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Numeracy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Numeracy
+//
+//  Created by Maxim Shchetynin on 03.03.14.
+//  Copyright (c) 2014 Elena Kukuruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NumeracyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NumeracyAppDelegate class]));
+    }
+}
